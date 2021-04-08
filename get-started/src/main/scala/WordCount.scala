315e46c9b9eeced65ebaf572ee28992b7ab6fab5
kodjo <kodjo@accelextech.com>
2021-04-08 21:33:30 +0200

    Adding packaging section to readme

--- get-started/src/main/scala/WordCount.scala
+++ get-started/src/main/scala/WordCount.scala
@@ -17,42 +17,44 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder()
       .appName(s"WordCount")
-      //.master("spark://locahost:7077")
-      //.master("local[*]")
+      // .master("spark://localhost:7077")
+      // .master("local[*]")
       .getOrCreate()
 
-    val filePath = "ulysses.txt"
-    logger.info(s"loading text from $filePath ...")
-    val content = Source.fromFile(filePath) // reading file content
-    val pattern = "([A-Z]+)".r // Set up words filtering regex. We only keep words
+    for (_ <- 1 to 100) {
+      val filePath = "ulysses.txt"
+      logger.info(s"loading text from $filePath ...")
+      val content = Source.fromFile(filePath) // reading file content
+      val pattern = "([A-Z]+)".r // Set up words filtering regex. We only keep words
 
-    logger.info("tokenizing the input text...")
-    val tokens = pattern.findAllIn(content.mkString.toUpperCase) // extracting words from the above text
-      .matchData.map(_.group(1)).toSeq
-      .zipWithIndex
+      logger.info("tokenizing the input text...")
+      val tokens = pattern.findAllIn(content.mkString.toUpperCase) // extracting words from the above text
+        .matchData.map(_.group(1)).toSeq
+        .zipWithIndex
 
-    logger.info("converting the tokens into dataframe...")
-    val tokensDf = spark.createDataFrame(tokens) // creating DataFrame from above Seq
-      .toDF("token", "id")
-      .select("token")
-    tokensDf.show(100, truncate = false)
+      logger.info("converting the tokens into dataframe...")
+      val tokensDf = spark.createDataFrame(tokens) // creating DataFrame from above Seq
+        .toDF("token", "id")
+        .select("token")
+      tokensDf.show(100, truncate = false)
 
-    logger.info("counting the occurrence of each word...")
-    val wordCountDf = tokensDf.where(length(col("token")) > 2) // we keep only words with length greater than 2
-      .groupBy("token") // counting the words using groupBy() instruction
-      .count()
-      .orderBy(desc("count"))
+      logger.info("counting the occurrence of each word...")
+      val wordCountDf = tokensDf.where(length(col("token")) > 2) // we keep only words with length greater than 2
+        .groupBy("token") // counting the words using groupBy() instruction
+        .count()
+        .orderBy(desc("count"))
 
-    logger.info("displaying the dataframe...")
-    wordCountDf.show(100, truncate = false)
+      logger.info("displaying the dataframe...")
+      wordCountDf.show(100, truncate = false)
+    }
 
-//    logger.info("saving the dataframe in csv format...")
-//    wordCountDf.repartition(1) // grouping the data into 1 partition
-//      .write.mode(SaveMode.Overwrite)
-//      .csv("tokens.csv")
-//
-//    logger.info("saving the dataframe in parquet format...")
-//    wordCountDf.write.mode(SaveMode.Overwrite)
-//      .parquet("tokens.parquet")
+    //    logger.info("saving the dataframe in csv format...")
+    //    wordCountDf.repartition(1) // grouping the data into 1 partition
+    //      .write.mode(SaveMode.Overwrite)
+    //      .csv("tokens.csv")
+    //
+    //    logger.info("saving the dataframe in parquet format...")
+    //    wordCountDf.write.mode(SaveMode.Overwrite)
+    //      .parquet("tokens.parquet")
   }
 }
