b90ed9171d10a5caab40ea4915520cbea270f975
wopoadi <kodjo@osekoo.com>
2023-06-04 17:28:22 +0200

    Fix spark-submit issue run the code with a docker to solve the network issue

---
+++ get-started/start-job.sh
@@ -0,0 +1,4 @@
+docker build -t spark-app .
+
+docker-compose up spark-worker -d
+docker-compose up spark-app
--- get-started/submit.bat
+++
@@ -1,9 +0,0 @@
-
-spark-submit ^
-  --master spark://localhost:7077 ^
-  --deploy-mode client ^
-  --executor-cores 4 ^
-  --num-executors 1 ^
-  --files ./ulysses.txt ^
-  --class WordCount ^
- target/scala-2.12/get-started_2.12-0.1.jar
